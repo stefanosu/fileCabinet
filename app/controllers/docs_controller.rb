2b34c03364d31b337a7fc832ed36ca192399c787
Stefanos <stefanosu422@gmail.com>
2020-10-20 14:43:46 -0400

    added form and docs view

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,2 +1,42 @@
 class DocsController < ApplicationController
+
+def index 
+
+end
+
+
+def show 
+
+end
+
+def new 
+
+end
+
+def create 
+
+end
+
+def edit 
+
+end
+
+def update  
+
+end
+
+def  destroy
+
+end
+
+private 
+
+def find_doc
+  
+end
+
+def docs_params 
+  
+end
+
 end

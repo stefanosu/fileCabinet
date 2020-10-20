5bbb23d1549413d1fae2138b29f86ac03df86fac
Stefanos <stefanosu422@gmail.com>
2020-10-20 15:46:58 -0400

    edit and destroy docs

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -28,11 +28,16 @@ def edit
 end
 
 def update  
-
+  if @doc.update(doc_params)
+    redirect_to @doc 
+  else 
+    render 'edit'
+  end
 end
 
-def  destroy
-
+def destroy
+  @doc.destroy 
+  redirect_to docs_path 
 end
 
 private 
